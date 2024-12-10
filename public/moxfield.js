ad207f725bc049758333217e4a5c0f9f5a939f61
Paul Vacey <pvacey@gmail.com>
2024-12-10 09:06:22 -0600

    initial commit

---
+++ public/moxfield.js
@@ -0,0 +1,181 @@
+const moxfieldInput = document.getElementById('moxfieldInput');
+const inbox = document.getElementById('inbox');
+
+moxfieldInput.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter' && !e.shiftKey) {
+        processURL();
+        e.preventDefault(); // Prevents default action of Enter key
+    }
+});
+
+function sizeDown() {
+    document.getElementById('imageSize').value++
+    resizeImages()
+}
+
+function sizeUp() {
+    document.getElementById('imageSize').value--
+    resizeImages()
+}
+
+function resizeImages(event) {
+
+    slider = document.getElementById('imageSize');
+    var width = Math.floor((document.getElementById('deck').getBoundingClientRect().width)/slider.value)-1;
+    document.documentElement.style.setProperty('--image-width', `${width}px`);
+    document.documentElement.style.setProperty('--image-radius', `${width *.05  }px`);
+
+    var height = document.getElementById('nav').offsetHeight;
+    document.documentElement.style.setProperty('--nav-height', `${height}px`);
+}
+
+// resize when window size changes
+onresize = () => {resizeImages()};
+// resize when when the slider input changes
+document.getElementById('imageSize').addEventListener("input", () => {resizeImages()});
+// resize for small screens
+if (document.getElementById('deck').getBoundingClientRect().width < 500) {
+    document.getElementById('imageSize').value = 2;
+}
+
+
+function processURL() {
+    const message = moxfieldInput.value.trim();
+    if (message !== '') {
+        const urlMatch = message.match(/https:\/\/www\.moxfield\.com\/decks\/([^\/]+)/)
+        if (urlMatch.length == 2) {
+            const deckID = urlMatch[1];
+            console.log(`valid moxfield URL, deck ID: ${deckID}`)
+            loadDeck(deckID)
+            localStorage.setItem("deckID", deckID);
+        }            
+    }
+    moxfieldInput.value = '';
+}
+
+function loadDeck(id) {
+    document.getElementById('deck').innerHTML = '';
+    fetch(`https://corsproxy.io/?url=https://api2.moxfield.com/v3/decks/all/${id}`)
+    .then(response => response.json())
+    .then(data => loadImages(data))
+    .catch(error => console.error('Error:', error));
+}
+
+const cardTypes = {
+    0: 'Commander',
+    1: 'Battle',
+    2: 'Planeswalker',
+    3: 'Creature',
+    4: 'Sorcery',
+    5: 'Instant',
+    6: 'Artifact',
+    7: 'Enchantment',
+    8: 'Land'
+}
+
+function loadImages(deckData) {
+    
+    const board = {};
+
+    Object.entries(deckData.boards.commanders.cards).forEach(([_, card]) => {
+        card.card.type = 0;
+        console.log(card.card.type)
+        if (!(card.card.type in board)) {
+            board[card.card.type] = [];
+        }
+        board[card.card.type].push(card)
+    });
+
+    Object.entries(deckData.boards.mainboard.cards).forEach(([_, card]) => {
+        if (!(card.card.type in board)) {
+            board[card.card.type] = [];
+        }
+        board[card.card.type].push(card)
+    });
+
+
+    Object.entries(board).forEach(([cardType, cardArray]) => {
+        
+        const typeBox = document.createElement('div');
+        typeBox.className = 'typeBox';
+    
+        const header = document.createElement('div');
+        header.className = 'cardType';
+        header.textContent  = cardTypes[cardType]
+        deck.appendChild(header);
+        deck.appendChild(typeBox);
+
+        const extractedData = [];
+        const layoutExceptions = ['adventure', 'split']
+        
+        Object.entries(cardArray).forEach(([_, data]) => {
+            let cardID = data.card.id
+            if (data.card.card_faces.length > 0){
+                cardID = `face-${data.card.card_faces[0].id}`
+                console.log(data.card.layout)
+                if (layoutExceptions.includes(data.card.layout)) {
+                    cardID = data.card.id
+                }
+                console.log(data)
+            }
+            extractedData.push({
+                id: cardID, 
+                name: data.card.name
+            });
+        });
+
+        extractedData.sort((a,b) => a.name > b.name);
+        extractedData.map((card) => addImage(card.id, card.name, typeBox));
+
+    });
+    
+    resizeImages()
+}
+
+function addImage(id, name, typeBox) {
+    const imgBox = document.createElement('div');
+    imgBox.className = 'imgBox'
+    const cardImg = document.createElement('img');
+    cardImg.src = `https://assets.moxfield.net/cards/card-${id}-normal.webp`;
+    cardImg.alt = name
+    cardImg.onclick = imageClick;
+    imgBox.appendChild(cardImg)
+    typeBox.appendChild(imgBox);
+}
+
+async function imageClick(event) {
+    console.log('clicked image')
+    console.log(event.target.src)
+    event.target.className = 'clicked';
+    setTimeout(() => {
+        event.target.classList.remove('clicked');
+    }, 2000);
+
+    const response = await fetch(`${window.location}emit`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: "POST",
+		body: JSON.stringify({
+			image_url: event.target.src
+		}),
+	});
+    if (response.status === 401) {
+        window.location = window.location + 'login'
+    }
+    
+}
+
+async function checkLogin() {
+    const response = await fetch(`${window.location}auth-status`);
+    if (response.status === 401) {
+        window.location = window.location + 'login'
+    } 
+}
+
+checkLogin()
+
+const deckID = localStorage.getItem("deckID");
+if (deckID) {
+    loadDeck(deckID)
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,137 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { AutoRouter, json, withCookies} from 'itty-router'
+import cookieSigner from 'cookie-signature'
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+
+const router = AutoRouter()
+
+function validateCookies(cookies, secret) {
+	let valid = false
+	if ('user_id' in cookies && 'servers' in cookies) {
+		if (cookieSigner.unsign(cookies.user_id, secret) !== false && cookieSigner.unsign(cookies.servers, secret) !== false) {
+			valid = true
+		}
+	}
+	return valid
+}
+
+function ensureAuth(request, secret) {
+	if (validateCookies(request.cookies, secret) !== true){
+		return new Response(null, {status: 401})
+	}
+}
+
+router.get('/login', async (request, env) => {
+	return Response.redirect(`https://discord.com/oauth2/authorize?client_id=${env.DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(env.DISCORD_REDIRECT_URI)}&scope=identify`)
+});
+
+
+router.get('/callback',  withCookies, async (request, env) => {
+
+	// take the code from the redirect and request an access token
+	let response = await fetch('https://discord.com/api/v10/oauth2/token', {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		method: "POST",
+		body: new URLSearchParams({
+			client_id: env.DISCORD_CLIENT_ID,
+			client_secret: env.DISCORD_CLIENT_SECRET,
+			code: request.query.code,
+			grant_type: 'authorization_code',
+			redirect_uri: env.DISCORD_REDIRECT_URI
+		}),
+	});
+	const authResp = await response.json();
+	console.log(authResp)
+
+	// if (!response.ok) {
+	// 	throw new Error(`HTTP error! status: ${response.status}`);
+	// }
+
+	// get user's ID 
+	response = await fetch('https://discord.com/api/v10/users/@me', {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${authResp.access_token}`
+		}
+	});
+	const user = await response.json();
+	console.log(user)
+
+	// get a list of guild IDs for the user
+	response = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${authResp.access_token}`
+		}
+	});
+	const userGuildData = await response.json();
+	const userGuildIDs = userGuildData.map((guild) => guild.id)
+
+	// get a list of guild IDs for the bot
+	response = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`
+		}
+	});
+	const botGuildData = await response.json();
+
+	// determine which guild IDs the user and bot both belong to, sign it and add to response
+	const authorizedGuildIDs = [];
+	botGuildData.map((guild) => {
+		if (userGuildIDs.includes(guild.id)) {
+			authorizedGuildIDs.push(guild.id)
+		}
+	});
+
+	console.log(request)
+	const r = new Response(null, {status: 302});
+	r.headers.append('Location', request.url.split(request.route)[0]);
+	r.headers.append('Set-Cookie', `user_id=${cookieSigner.sign(user.id, env.DISCORD_CLIENT_SECRET)}; secure; HttpOnly; SameSite=Strict; Max-Age=3600`);
+	r.headers.append('Set-Cookie', `servers=${cookieSigner.sign(authorizedGuildIDs.join('_'), env.DISCORD_CLIENT_SECRET)}; secure; HttpOnly; SameSite=Strict; Max-Age=3600`);
+	return r;
+
+});
+
+router.post('/emit', withCookies, async (request, env) => {
+	const unauthorized = ensureAuth(request, env.DISCORD_CLIENT_SECRET);
+	if (unauthorized) {
+		return unauthorized
+	} 
+
+	const reqJSON = await request.json()
+	console.log(reqJSON)
+
+	const response = await fetch(`https://discord.com/api/v10/channels/${env.DISCORD_CHANNEL_ID}/messages`, {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`
+		},
+		method: "POST",
+		body: JSON.stringify({
+			// content: reqJSON.image_url,
+			content: `<@${request.cookies.user_id.split('.')[0]}> casts a [spell](${reqJSON.image_url})`,
+			allowed_mentions: {
+				parse: ["users"]
+			}
+		})
+	});
+	const whatever = await response.json()
+	console.log(whatever)
+	return 'ok'
+});
+
+router.get('/auth-status', withCookies, (request, env) => {
+	const unauthorized = ensureAuth(request, env.DISCORD_CLIENT_SECRET);
+	if (unauthorized) {
+		return unauthorized
+	} else {
+		return new Response(null, {status: 200})
+	}
+});
+
+
+router.all('*', () => new Response('Not Found', { status: 404 }))
+
+export default router
