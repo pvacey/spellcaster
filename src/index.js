1fc3790b85bc999f7e4c6da7b8af0dd864b8cb78
Dan Cigrang <danielcigrang@me.com>
2024-12-11 03:34:45 +0000

    refactor server

--- src/index.js
+++ src/index.js
@@ -1,152 +1,154 @@
-import { AutoRouter, json, withCookies} from 'itty-router'
-import cookieSigner from 'cookie-signature'
+import { AutoRouter, withCookies, withContent } from 'itty-router';
+import cookieSigner from 'cookie-signature';
 
+const router = AutoRouter();
 
-const router = AutoRouter()
-
+/**
+ * Checks that the user_id and servers cookies are both present and valid.
+ * A valid cookie is one that can be successfully unsign()ed with the given secret.
+ * @param {Record<string, string>} cookies - The cookies from the request
+ * @param {string} secret - The secret used to sign the cookies
+ * @returns {boolean} - Whether the cookies are valid
+ */
 function validateCookies(cookies, secret) {
-	let valid = false
-	if ('user_id' in cookies && 'servers' in cookies) {
-		if (cookieSigner.unsign(cookies.user_id, secret) !== false && cookieSigner.unsign(cookies.servers, secret) !== false) {
-			valid = true
-		}
-	}
-	return valid
+	return cookieSigner.unsign(cookies?.user_id, secret) && cookieSigner.unsign(cookies?.servers, secret);
 }
 
+/**
+ * Checks that the user_id and servers cookies are both present and valid.
+ * A valid cookie is one that can be successfully unsign()ed with the given secret.
+ * @param {Record<string, string>} request.cookies - The cookies from the request
+ * @param {string} secret - The secret used to sign the cookies
+ * @returns {boolean} - Whether the cookies are valid
+ */
 function ensureAuth(request, secret) {
-	if (validateCookies(request.cookies, secret) !== true){
-		return new Response(null, {status: 401})
-	}
+	return validateCookies(request?.cookies, secret);
 }
 
-router.get('/login', async (request, env) => {
-	return Response.redirect(`https://discord.com/oauth2/authorize?client_id=${env.DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(env.DISCORD_REDIRECT_URI)}&scope=identify+guilds`)
+// login page redirect
+router.get('/login', async (_, env) => {
+	const discord_redirect = new URL('https://discord.com/api/oauth2/authorize');
+	discord_redirect.searchParams.set('client_id', env.DISCORD_CLIENT_ID);
+	discord_redirect.searchParams.set('response_type', 'code');
+	discord_redirect.searchParams.set('redirect_uri', env.DISCORD_REDIRECT_URI);
+	discord_redirect.searchParams.set('scope', 'identify guilds');
+
+	return Response.redirect(discord_redirect, 302);
 });
 
-
-router.get('/callback',  withCookies, async (request, env) => {
-
+// callback page
+router.get('/callback', withCookies, async ({ query, url, route }, env) => {
 	// take the code from the redirect and request an access token
-	let response = await fetch('https://discord.com/api/v10/oauth2/token', {
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		method: "POST",
-		body: new URLSearchParams({
-			client_id: env.DISCORD_CLIENT_ID,
-			client_secret: env.DISCORD_CLIENT_SECRET,
-			code: request.query.code,
-			grant_type: 'authorization_code',
-			redirect_uri: env.DISCORD_REDIRECT_URI
-		}),
-	});
-	const authResp = await response.json();
-	console.log(authResp)
+	try {
+		const oauth_response = await fetch('https://discord.com/api/v10/oauth2/token', {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			method: 'POST',
+			body: new URLSearchParams({
+				client_id: env.DISCORD_CLIENT_ID,
+				client_secret: env.DISCORD_CLIENT_SECRET,
+				code: query.code,
+				grant_type: 'authorization_code',
+				redirect_uri: env.DISCORD_REDIRECT_URI,
+			}),
+		});
 
-	// if (!response.ok) {
-	// 	throw new Error(`HTTP error! status: ${response.status}`);
-	// }
+		const authResp = await oauth_response.json();
 
-	// get user's ID 
-	response = await fetch('https://discord.com/api/v10/users/@me', {
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${authResp.access_token}`
-		}
-	});
-	const user = await response.json();
-	console.log(user)
+		// get user's ID
+		const user_response = await fetch('https://discord.com/api/v10/users/@me', {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${authResp.access_token}`,
+			},
+		});
+		const { id: user_id } = await user_response.json();
 
-	// get a list of guild IDs for the user
-	response = await fetch('https://discord.com/api/v10/users/@me/guilds', {
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${authResp.access_token}`
-		}
-	});
-	const userGuildData = await response.json();
-	const userGuildIDs = userGuildData.map((guild) => guild.id)
+		// get a list of guild IDs for the user
+		const guild_response = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${authResp.access_token}`,
+			},
+		});
+		const userGuildData = await guild_response.json();
+		const userGuildIDs = userGuildData.map(({ id }) => id);
 
-	// get a list of guild IDs for the bot
-	response = await fetch('https://discord.com/api/v10/users/@me/guilds', {
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`
-		}
-	});
-	const botGuildData = await response.json();
+		// get a list of guild IDs for the bot
+		const bot_guild_response = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`,
+			},
+		});
+		const botGuildData = await bot_guild_response.json();
 
-	// determine which guild IDs the user and bot both belong to, sign it and add to response
-	const authorizedGuildIDs = [];
-	botGuildData.map((guild) => {
-		if (userGuildIDs.includes(guild.id)) {
-			authorizedGuildIDs.push(guild.id)
-		}
-	});
+		// determine which guild IDs the user and bot both belong to, sign it and add to response
+		const authorizedGuildIDs = botGuildData.map(({ id }) => {
+			if (userGuildIDs.includes(id)) {
+				return id;
+			}
+		});
 
-	console.log(request)
-	const r = new Response(null, {status: 302});
-	r.headers.append('Location', request.url.split(request.route)[0]);
-	r.headers.append('Set-Cookie', `user_id=${cookieSigner.sign(user.id, env.DISCORD_CLIENT_SECRET)}; secure; HttpOnly; SameSite=Strict; Max-Age=3600`);
-	r.headers.append('Set-Cookie', `servers=${cookieSigner.sign(authorizedGuildIDs.join('_'), env.DISCORD_CLIENT_SECRET)}; secure; HttpOnly; SameSite=Strict; Max-Age=3600`);
-	return r;
+		const [location, _] = url.split(route);
+		const redirect_response = new Response(null, { status: 302 });
+		redirect_response.headers.append('Location', location);
+		redirect_response.headers.append(
+			'Set-Cookie',
+			`user_id=${cookieSigner.sign(user_id, env.DISCORD_CLIENT_SECRET)}; secure; HttpOnly; SameSite=Strict; Max-Age=3600`
+		);
 
+		redirect_response.headers.append(
+			'Set-Cookie',
+			`servers=${cookieSigner.sign(
+				authorizedGuildIDs.join('_'),
+				env.DISCORD_CLIENT_SECRET
+			)}; secure; HttpOnly; SameSite=Strict; Max-Age=3600`
+		);
+
+		return redirect_response;
+	} catch (error) {
+		return new Response(error, { status: 500 });
+	}
 });
 
-router.post('/emit', withCookies, async (request, env) => {
-	const unauthorized = ensureAuth(request, env.DISCORD_CLIENT_SECRET);
-	if (unauthorized) {
-		return unauthorized
-	} 
+router.post('/emit', withCookies, withContent, async (request, env) => {
+	const authorized = ensureAuth(request, env.DISCORD_CLIENT_SECRET);
+	if (!authorized) {
+		return new Response(null, { status: 401 });
+	}
 
-	const reqJSON = await request.json()
-	console.log(reqJSON)
-
-	// go fetch info by ID from 
+	// go fetch info by ID from
 	/// https://api2.moxfield.com/v3/cards/editions/${cardID}
 
-	const simpleMessage = {
-		content: `<@${request.cookies.user_id.split('.')[0]}> casts a [spell](${reqJSON.image_url})`,
+	const body = JSON.stringify({
+		content: `<@${request.cookies.user_id.split('.')[0]}> casts a [spell](${request.content.image_url})`,
 		allowed_mentions: {
-			parse: ["users"]
-		}
-	}
+			parse: ['users'],
+		},
+	});
 
-	const response = await fetch(`https://discord.com/api/v10/channels/${env.DISCORD_CHANNEL_ID}/messages`, {
+	await fetch(`https://discord.com/api/v10/channels/${env.DISCORD_CHANNEL_ID}/messages`, {
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`
+			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`,
 		},
-		method: "POST",
-		body: JSON.stringify(simpleMessage)
+		method: 'POST',
+		body,
 	});
-	const whatever = await response.json()
-	console.log(whatever)
-	return 'ok'
+
+	return new Response(null, { status: 200 });
 });
 
 router.get('/auth-status', withCookies, (request, env) => {
-	const unauthorized = ensureAuth(request, env.DISCORD_CLIENT_SECRET);
-	if (unauthorized) {
-		return unauthorized
-	} else {
-		return new Response(null, {status: 200})
+	const authorized = ensureAuth(request, env.DISCORD_CLIENT_SECRET);
+	if (!authorized) {
+		return new Response(null, { status: 401 });
 	}
+	return new Response(null, { status: 200 });
 });
 
+router.all('*', () => new Response('Not Found', { status: 404 }));
 
-router.all('*', () => new Response('Not Found', { status: 404 }))
-
-export default router
-
-
-// let example = 
-//   {
-//     description: "",
-//     fields: [],
-//     image: {
-//       url: "https://images-ext-1.discordapp.net/external/MSeHQK3-X4dckR9yJ_k-ZrHR7sQBa9JegVb2arpjvNw/https/spelltable.danielcigrang.workers.dev/_drc?format=webp&width=489&height=682"
-//     },
-//     color: 5462587
-//   }
+export default router;
