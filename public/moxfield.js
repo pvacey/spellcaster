668e58c6dbf019e843439b7abb4c309c6fb9f5f5
Paul Vacey <pvacey@gmail.com>
2024-12-12 21:30:53 -0600

    bug with sorting, previous method only worked in firefox

--- public/moxfield.js
+++ public/moxfield.js
@@ -127,7 +127,20 @@ function loadImages(data) {
         deck.appendChild(content);
 
         // sort the cards aplhabetically
-        cArray.sort((a,b) => a.name > b.name);
+        // cArray.sort((a,b) => a.name > b.name);
+        cArray.sort((a, b) => {
+            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+          
+            // names must be equal
+            return 0;
+          });
 
         // add images to the content div
         cArray.map((card) => {
