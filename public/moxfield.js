05c9da95ff7ef309153c10f05195cc1ec32afd93
Paul Vacey <pvacey@gmail.com>
2024-12-14 16:32:01 -0600

    idk

--- public/moxfield.js
+++ public/moxfield.js
@@ -106,84 +106,6 @@ async function loadDeck(id) {
 }
 
 function loadImages(data) {
-<<<<<<< HEAD
-    
-    // sorting bin, an array per card type
-    const bin = {}
-    Object.keys(cardTypes).forEach((typeID) => bin[typeID] = []);
-    // collect commanders and sort mainboard cards based on type
-    Object.values(data.commanders.cards).forEach(({card}) => bin[0].push(card));
-    Object.values(data.mainboard.cards).forEach(({card}) => bin[card.type].push(card));
-
-    Object.entries(bin).forEach(([cType, cArray]) => {
-        // skip it if we don't have that type
-        if (cArray.length == 0) {
-            return
-        }
-    
-        const header = document.createElement('div');
-        header.className = 'cardType';
-        header.textContent  = cardTypes[cType]
-        deck.appendChild(header);
-
-        const content = document.createElement('div');
-        content.className = 'cardBox';
-        deck.appendChild(content);
-
-        // sort the cards aplhabetically
-        // cArray.sort((a,b) => a.name > b.name);
-        cArray.sort((a, b) => {
-            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
-            if (nameA < nameB) {
-              return -1;
-            }
-            if (nameA > nameB) {
-              return 1;
-            }
-          
-            // names must be equal
-            return 0;
-          });
-
-        // add images to the content div
-        cArray.map((card) => {
-
-            const imgBox = document.createElement('div');
-            imgBox.className = 'imgBox';
-            const cardImg = document.createElement('img');
-            cardImg.className = 'card';
-            cardImg.onclick = (event) => discordMessage(event.target,'casts');
-            cardImg.alt = card.name;
-
-            // assume it's a one-sided card
-            cardImg.src = `https://assets.moxfield.net/cards/card-${card.id}-normal.webp`;
-            cardImg.dataset.type = cardTypes[cType];
-            cardImg.dataset.front = card.id;
-            cardImg.dataset.back = '';
-            // check for double faced cards but exclude certain types
-            if (card.card_faces.length > 0 && !(['adventure', 'split'].includes(card.layout))) {
-                cardImg.src = `https://assets.moxfield.net/cards/card-face-${card.card_faces[0].id}-normal.webp`;
-                cardImg.dataset.front = card.card_faces[0].id
-                cardImg.dataset.back = card.card_faces[1].id
-            }
-
-            imgBox.appendChild(cardImg)
-            content.appendChild(imgBox);
-        });
-    });
-    
-    // make sure they fit the window properly
-    resizeImages()
-}
-
-async function discordMessage(card, verb) {
-    // css animation
-    card.className.classList.add('clicked');
-    setTimeout(() => {
-        card.classList.remove('clicked');
-    }, 250);
-=======
 	// sorting bin, an array per card type
 	const bin = {};
 	Object.keys(cardTypes).forEach((typeID) => (bin[typeID] = []));
@@ -264,63 +186,11 @@ async function imageClick(event) {
 		front_id: event.target.dataset.front,
 		back_id: event.target.dataset.back,
 	});
->>>>>>> 0233f27f67575dd4f2906d4072e485595f183e36
 
 	const response = await fetch('/emit', {
 		headers: {
 			'Content-Type': 'application/json',
 		},
-<<<<<<< HEAD
-		method: "POST",
-		body: JSON.stringify({
-            verb: verb,
-            type: card.dataset.type,
-            front_id: card.dataset.front,
-			back_id: card.dataset.back
-		}),
-	});
-    if (response.status === 401) {
-        window.location = window.location + 'login'
-    }
-}
-
-
-
-function hideMenu() {
-    document.getElementById("contextMenu").style.display = "none"
-}
-
-function rightClick(e) {
-    e.preventDefault();
-    if (e.target.className !== 'card') {
-        return
-    }
-
-    targetCard = e.target;
-
-    if (document.getElementById("contextMenu").style.display == "block")
-        hideMenu()
-    else {
-        let menu = document.getElementById("contextMenu")
-        menu.style.display = 'block';
-        menu.style.left = e.pageX + "px";
-        menu.style.top = e.pageY + "px";
-    }
-}
-
-function contextMenuClick({target}) {
-    const verb = target.textContent + 's';
-    discordMessage(targetCard, verb)
-
-}
-
-document.onclick = hideMenu;
-document.oncontextmenu = rightClick;
-const contextMenu = document.getElementById('contextMenu');
-contextMenu.onclick = contextMenuClick;
-let targetCard = null;
-
-=======
 		method: 'POST',
 		body,
 	});
@@ -328,4 +198,3 @@ let targetCard = null;
 		window.location = 'login';
 	}
 }
->>>>>>> 0233f27f67575dd4f2906d4072e485595f183e36
