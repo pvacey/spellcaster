054f1de4907adf648db7ab2d30e87188b481d45d
Dan Cigrang <danielcigrang@me.com>
2024-12-15 20:04:13 -0600

    update test to give ideas on how it would work

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,19 +2,19 @@ import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloud
 import { describe, it, expect } from 'vitest';
 import worker from '../src';
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new Request('http://example.com');
+describe('Spellcaster™ worker', () => {
+	it('responds with failed login', async () => {
+		const request = new Request('http://localhost:8787/auth-status');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.json()).toMatchObject({ error: 'Secret key must be provided.', status: 500 });
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch(request, env, ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('responds with failed login (integration style)', async () => {
+		const response = await SELF.fetch('http://localhost:8787/auth-status');
+		expect(await response.json()).toMatchObject({ error: 'Secret key must be provided.', status: 500 });
 	});
 });
